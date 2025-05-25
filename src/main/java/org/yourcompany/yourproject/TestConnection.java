099b963d3a1f2fd2c542742c6ef565bfaa50f4c6
Aneesh Agarwal <911aneesh@gmail.com>
2025-05-25 14:57:41 -0700

    first draft

---
+++ src/main/java/org/yourcompany/yourproject/TestConnection.java
@@ -0,0 +1,104 @@
+package org.yourcompany.yourproject;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import oracle.jdbc.OracleConnection;
+import oracle.jdbc.pool.OracleDataSource;
+
+public class TestConnection {
+    // The recommended format of a connection URL is:
+    // "jdbc:oracle:thin:@<DATABASE_NAME_LOWERCASE>_low?TNS_ADMIN=<PATH_TO_WALLET>"
+    // where
+    // <DATABASE_NAME_LOWERCASE> is your database name in lowercase
+    // and
+    // <PATH_TO_WALLET> is the path to the connection wallet on your machine.
+    // NOTE: on a Mac, there's no C: drive...
+    private static final Dotenv env = Dotenv.load();
+    private static final String DB_URL      = env.get("DB_URL");
+    private static final String DB_USER     = env.get("DB_USER");
+    private static final String DB_PASSWORD = env.get("DB_PASSWORD");
+    // This method creates a database connection using
+    // oracle.jdbc.pool.OracleDataSource.
+    public static void main(String args[]) throws SQLException {
+        Properties info = new Properties();
+
+        System.out.println("Initializing connection properties...");
+        info.put(OracleConnection.CONNECTION_PROPERTY_USER_NAME, DB_USER);
+        info.put(OracleConnection.CONNECTION_PROPERTY_PASSWORD, DB_PASSWORD);
+        info.put(OracleConnection.CONNECTION_PROPERTY_DEFAULT_ROW_PREFETCH, "20");
+
+        System.out.println("Creating OracleDataSource...");
+        OracleDataSource ods = new OracleDataSource();
+
+        System.out.println("Setting connection properties...");
+        ods.setURL(DB_URL);
+        ods.setConnectionProperties(info);
+
+        // With AutoCloseable, the connection is closed automatically
+        try (OracleConnection connection = (OracleConnection) ods.getConnection()) {
+            System.out.println("Connection established!");
+            // Get JDBC driver name and version
+            DatabaseMetaData dbmd = connection.getMetaData();
+            System.out.println("Driver Name: " + dbmd.getDriverName());
+            System.out.println("Driver Version: " + dbmd.getDriverVersion());
+            // Print some connection properties
+            System.out.println(
+                "Default Row Prefetch Value: " + connection.getDefaultRowPrefetch()
+            );
+            System.out.println("Database username: " + connection.getUserName());
+            System.out.println();
+            // Perform some database operations
+            insertTA(connection);
+            printInstructors(connection);
+        } catch (Exception e) {
+            System.out.println("CONNECTION ERROR:");
+            System.out.println(e);
+        }
+    }
+
+    // Inserts another TA into the Instructors table.
+    public static void insertTA(Connection connection) throws SQLException {
+        System.out.println("Preparing to insert TA into Instructors table...");
+        // Statement and ResultSet are AutoCloseable and closed automatically. 
+        try (Statement statement = connection.createStatement()) {
+            try (
+                ResultSet resultSet = statement.executeQuery(
+                    "INSERT INTO INSTRUCTORS VALUES (10, 'James josf', 'TA')"
+                )
+            ) {}
+        } catch (Exception e) {
+            System.out.println("ERROR: insertion failed.");
+            System.out.println(e);
+        }
+    }
+
+    // Displays data from Instructors table.
+    public static void printInstructors(Connection connection) throws SQLException {
+        // Statement and ResultSet are AutoCloseable and closed automatically. 
+        try (Statement statement = connection.createStatement()) {
+            try (
+                ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM INSTRUCTORS"
+                )
+            ) {
+                System.out.println("INSTRUCTORS:");
+                System.out.println("I_ID\tI_NAME\t\tI_ROLE");
+                while (resultSet.next()) {
+                    System.out.println(
+                        resultSet.getString("I_ID") + "\t"
+                        + resultSet.getString("I_NAME") + "\t"
+                        + resultSet.getString("I_ROLE")
+                    );
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR: selection failed.");
+            System.out.println(e);
+        }
+    }
+}
